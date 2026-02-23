2be8f13f0989b4463f21d920c5ddc2ba33ced0d6
Sourov bhuiyan <sourovofficial83@gmail.com>
2026-02-23 12:51:13 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname)));
 
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/premium_blog';
 
